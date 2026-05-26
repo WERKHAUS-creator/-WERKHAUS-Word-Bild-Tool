44f5c3b1ee43ff1b8ce458d51773a76e0182fd47
Stefan Hubenschmid <hubenschmid@MacStudio-von-Stefan.local>
2026-05-26 06:08:19 +0200

    chore: bump word image tool to 1.0.0.10

--- src/taskpane/projectFile.ts
+++ src/taskpane/projectFile.ts
@@ -9,7 +9,7 @@ const LEGACY_PROJECT_SCHEMA_VERSION = 1;
 const DEFAULT_DOCUMENT_BASIS = "Blanco-Dokument";
 const DEFAULT_OUTPUT_BASENAME = "werkhaus_bildprojekt";
 const DEFAULT_PREVIEW_SIZE_PX = 120;
-const DEFAULT_TOOL_VERSION = "1.0.0.9";
+const DEFAULT_TOOL_VERSION = "1.0.0.10";
 
 const DEFAULT_ANALYSIS_PROFILES = {
   allgemein: {
